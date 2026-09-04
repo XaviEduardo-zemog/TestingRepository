d043efe13da9469cc90ec03f48fca65d852a97d4
XaviEduardo <xavier.sanchez@zemog.com.mx>
2026-09-04 11:50:45 -0600

    Add CIS DB context and tidy summary styles
    Adds the EF Core persistence model for the CIS_DB RutasZam table, including the DbContext and entity used to map the SQL Server data source. This commit also removes a few explanatory comments from the executive summary Razor and CSS files to keep the implementation cleaner and more concise.

---
+++ Testing.Infrastructure/Persistence/CIS_DB/CisContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Testing.Infrastructure.Persistence.CIS_DB;
+
+public partial class CisContext : DbContext
+{
+    public CisContext()
+    {
+    }
+
+    public CisContext(DbContextOptions<CisContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<RutasZam> RutasZams { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=10.128.0.5;DataBase=CIS_DB;User Id=sqlalejandro;Password=Q7FozxB}qULd&Xy(;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.UseCollation("Latin1_General_CI_AI");
+
+        modelBuilder.Entity<RutasZam>(entity =>
+        {
+            entity.HasKey(e => e.Ruta);
+
+            entity.ToTable("RutasZam");
+
+            entity.Property(e => e.Ruta)
+                .HasMaxLength(250)
+                .IsUnicode(false);
+            entity.Property(e => e.Codigo)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Destino)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Kms).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.Origen)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Testing.Infrastructure/Persistence/CIS_DB/RutasZam.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Testing.Infrastructure.Persistence.CIS_DB;
+
+public partial class RutasZam
+{
+    public string Ruta { get; set; } = null!;
+
+    public string Codigo { get; set; } = null!;
+
+    public decimal Kms { get; set; }
+
+    public string Origen { get; set; } = null!;
+
+    public string Destino { get; set; } = null!;
+}
